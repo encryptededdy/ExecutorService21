7cd4093b58311b344a5e0f45eba0b069b2ee654a
Edward Zhang <edward@zhang.nz>
2019-04-26 17:18:16 +1200

    Try to determine max optimal threads

--- src/main/java/softeng751/g21/BenchmarkExecutorServiceFactory.java
+++ src/main/java/softeng751/g21/BenchmarkExecutorServiceFactory.java
@@ -3,21 +3,39 @@ package softeng751.g21;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BenchmarkExecutorServiceFactory {
     private static final int WORK_DIVISOR = 50;
+    private static final int BASELINE_AVERAGES = 4;
+    private static final double PERFORMANCE_CUTOFF = 1.1;
 
     public static void main(String[] args) {
         runBenchmarkAndGetExecutorService();
     }
 
     @SuppressWarnings("WeakerAccess")
-    public static void runBenchmarkAndGetExecutorService() {
+    public static ExecutorService runBenchmarkAndGetExecutorService() {
         int hwThreads = Runtime.getRuntime().availableProcessors();
-        for (int i = 1; i <= hwThreads * 512; i*=2) {
-            System.out.println("Tests with "+i+" threads took "+testWithThreads(i)+"ms");
+        double baseline = IntStream.range(0, BASELINE_AVERAGES).mapToLong(i -> testWithThreads(hwThreads)).average().orElse(0);
+        System.out.println("Average with availableProcessors: "+baseline+"ms");
+        int maxOptimalThreads = 0;
+        for (int i = hwThreads; i <= hwThreads * 512; i*=2) {
+            // Search for max threads before things start getting slow
+            double runTime = testWithThreads(i);
+            if (runTime < baseline * PERFORMANCE_CUTOFF) {
+                maxOptimalThreads = i;
+            } else {
+                break;
+            }
         }
+        System.out.println("Max optimal threads before slowdown is "+maxOptimalThreads);
+        return Executors.newFixedThreadPool(maxOptimalThreads);
     }
 
     private static long testWithThreads(int numOfThreads) {
